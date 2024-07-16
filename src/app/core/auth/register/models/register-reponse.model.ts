faec5acbbdf5bd03ecbf8c4efc54b1cfc976ab7a
BrunoFares <faresbruno04@gmail.com>
2024-07-16 16:34:52 +0300

    Initialised the models for the register feature.

---
+++ src/app/core/auth/register/models/register-reponse.model.ts
@@ -0,0 +1,22 @@
+export interface IRegisterResponse {
+    id: string,
+    createdTimestamp: number,
+    username: string,
+    enabled: boolean,
+    totp: boolean,
+    emailVerified: boolean,
+    firstName: string,
+    lastName:  string,
+    email: string,
+    disableableCredentialTypes: [],
+    requiredActions: [],
+    notBefore: number,
+    access: {
+      manageGroupMembership: boolean,
+      view: boolean,
+      mapRoles: boolean,
+      impersonate: boolean,
+      manage: boolean
+    },
+    attributes: null
+  }
