fb6db8e04c3e530135aa9d5c6f2a840c0ba022e6
BrunoFares <faresbruno04@gmail.com>
2024-07-18 03:09:54 +0300

    Set up routing for login, register, and create admin components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
+import { CreateAdminComponent } from './core/auth/create-admin/create-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'create-admin', component: CreateAdminComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
