32e4956e29c8f4227e07d037e9084e8e70a1ff6d
BrunoFares <faresbruno04@gmail.com>
2024-07-19 03:44:28 +0300

    Added some routing elements between the header and the login page.

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { ILoginRequest } from './models/login-request.model';
 import { LoginService } from './services/login.service';
 import { ILoginResponse } from './models/login-response.model';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,7 @@ export class LoginComponent {
   password: string = '';
   allowed: boolean = true;
 
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService, private router: Router) {}
 
   loginForm = new FormGroup({
     username: new FormControl(this.username, Validators.required),
@@ -28,7 +29,7 @@ export class LoginComponent {
     }
 
     this.loginService.login(request).subscribe((response: ILoginResponse) => {
-      console.log(response);
+      this.router.navigateByUrl('../');
     }, error => {
       this.allowed = false;
     });
