5b9095b14f0705847517ae6469269604c505ff83
BrunoFares <faresbruno04@gmail.com>
2024-08-17 21:38:35 +0300

    Reworked the entire shopping-cart component to make sure that it works correctly, without using AG-Grid because it is better design-wise.

--- src/app/features/shopping-cart/component/shopping-cart.component.ts
+++ src/app/features/shopping-cart/component/shopping-cart.component.ts
@@ -1,22 +1,49 @@
-import { Component } from '@angular/core';
-import { ColDef } from 'ag-grid-community';
+import { Component, OnInit } from '@angular/core';
+import { createStore, entries, del, update } from 'idb-keyval';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrl: './shopping-cart.component.scss'
 })
-export class ShoppingCartComponent {
-  rowData = [
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  ];
- 
-  colDefs: ColDef[] = [
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" }
-  ];
+export class ShoppingCartComponent implements OnInit {
+  rowData: any[] = [];
+  totalCost = 0;
+  shippingCost = 8.67;
+  promoCode!: string;
+  customStore: any;
+  quantity!: number;
+
+  changeQuantity(id: number) {
+    let i;
+    for (i = 0; i < this.rowData.length; i++) {
+      if (this.rowData[i][0] === id) break;
+    }
+    update(id, data => ({
+      title: this.rowData[i][1].title,
+      id: this.rowData[i][1].id,
+      price: this.rowData[i][1].price,
+      quantity: this.rowData[i][1].quantity,
+      category: this.rowData[i][1].category
+    }), this.customStore)
+    this.ngOnInit();
+  }
+
+  onRemove(id: number) {
+    del(id, this.customStore);
+    this.ngOnInit();
+  }
+
+  ngOnInit() {
+    this.customStore = createStore('ShoppingCartDB', 'ShoppingCartStore');
+
+    entries(this.customStore).then((data) => {
+      this.rowData = data.map(item => item);
+      this.totalCost = 0;
+
+      this.rowData.map(data => {
+        this.totalCost += data[1].price * data[1].quantity;
+      })
+    });
+  }
 }
