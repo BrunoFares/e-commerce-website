55e2c9a2eafb274ce2678984792d7db5e90e3024
BrunoFares <faresbruno04@gmail.com>
2024-08-17 15:29:21 +0300

    Implemented the search functionality into the header component.

--- src/app/core/app-shell/header/header.component.ts
+++ src/app/core/app-shell/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ListItemsService } from '../../../features/list-items/service/list-items.service';
+import { ListItemsComponent } from '../../../features/list-items/component/list-items.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +10,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   displayCategories = false;
+  searchQuery!: string;
 
-  constructor() {}
+  constructor(private router: Router) { }
+
+  onSearch() {
+    this.router.navigateByUrl('products').catch(() => {
+      this.router.navigateByUrl('../products')
+    })
+  }
 
   displayCats() {
     this.displayCategories = true;
